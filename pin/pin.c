b439245c6b6e49e81c5238be8684ee5e66f51c05
nbfhjgr <nbfhjgr@hotmail.com>
2015-12-09 19:53:08 +0100

    jgr 4th commit

--- pin/pin.c
+++ pin/pin.c
@@ -18,6 +18,7 @@ int diff1, diff2;
 
 int pin[9000], prob[9000], try[9000];
 
+//Prob Events von verschiedenem bit
 static int PIN_D1[10] = { 0, 4, 2, 2, 2, 2, 1, 1, 1, 1 };
 static int PIN_OtherD[10] = { 2, 2, 2, 2, 2, 2, 1, 1, 1, 1 };
 
@@ -52,9 +53,9 @@ void attack(void) {
 	/*>>>>  Aufgabe: Bestimmen die PIN                          <<<<*/
 	/*>>>>                                                      <<<<*/
 
-	int Off1[4], Off2[4];
-	int counts_dig[4][10];
-	int sum_counts_dig[4];
+	int Off1[4], Off2[4];		//Offset bits array
+	int counts_dig[4][10];		//Prob von jedem Ziffer in jedem bit Pin
+	int sum_counts_dig[4];		//Sum von jedem bit PIN
 	int index = 0;
 
 	int pins[100];
@@ -62,12 +63,14 @@ void attack(void) {
 
 	int i, j, x, y, k, l;
 
+	//initialization
 	for (i = 0; i < 4; i++)
 		for (j = 0; j < 10; j++)
 			counts_dig[i][j] = 0;
 
 	x = diff1;
 	y = diff2;
+
 
 	for (i = 3; i >= 0; i--) {
 		Off1[i] = x % 10;
@@ -79,12 +82,14 @@ void attack(void) {
 	for (i = 0; i < 4; i++)
 		sum_counts_dig[i] = 0;
 
+	// Berechnen Prob von Ziffer in 1. bit PIN
 	for (i = 0; i < 10; i++) {
 		counts_dig[0][i] = PIN_D1[i] * PIN_OtherD[(i + 10 - Off1[0]) % 10]
 				* PIN_OtherD[(i + 10 - Off2[0]) % 10];
 		sum_counts_dig[0] += counts_dig[0][i];
 	}
 
+	// Berechnen Prob von Ziffer in 2,3,4 bits PIN
 	for (k = 1; k < 4; k++)
 		for (i = 0; i < 10; i++) {
 			counts_dig[k][i] = PIN_OtherD[i]
@@ -93,6 +98,8 @@ void attack(void) {
 			sum_counts_dig[k] += counts_dig[k][i];
 		}
 
+
+	// Berechnen alle 9000 Probs
 	for (i = 1; i < 10; i++)
 		for (j = 0; j < 10; j++)
 			for (k = 0; k < 10; k++)
@@ -101,6 +108,7 @@ void attack(void) {
 							counts_dig[0][i] * counts_dig[1][j]
 									* counts_dig[2][k] * counts_dig[3][l];
 
+	// Suchen die 100 größte Prob Zifferen aus
 	memset(max_prob, 0, 101 * sizeof(float));
 	for (i = 0; i < 9000; i++) {
 		if (prob[i] > max_prob[1]) {
@@ -110,6 +118,7 @@ void attack(void) {
 		adjust(max_prob, pins);
 	}
 
+	// Berechnen die Prob von Attacks-Erfolg
 	for (i=0;i<100;i++)
 		sum+=pins[i];
 
