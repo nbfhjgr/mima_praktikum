0c641573026975ed59bf3692435829d2636a3385
nbfhjgr <nbfhjgr@hotmail.com>
2015-12-09 10:11:55 +0100

    jgr 4th commit

--- feal/feal.c
+++ feal/feal.c
@@ -14,8 +14,6 @@
 
 #include "feal.h"
 
-ubyte s1 = 0x01, s2 = 0xfe;
-
 static ubyte rotr(ubyte a) {
 	return ((a >> 2) | (a << 6)) & 0xff;
 }
@@ -39,33 +37,42 @@ static ubyte calc_f(ubyte u, ubyte v) {
 
 /* --------------------------------------------------------------------------- */
 
-ubyte bitsCheck(ubyte u, ubyte v) {
+ubyte s1 = 0x01, s2 = 0xfe;		//position von keys, noch zu bestimmen. Zunächst die 1. Position(niedrigste Position)
+
+// geben eine bestimmte bit(folgend bit) value von Feal_GS(u,v) zurück
+ubyte getBitValueOfFEAL(ubyte u, ubyte v) {
 	return (s1 << 1) & rotr(calc_f(u, v));
 }
 
-void bCheck(ubyte *u, ubyte *v) {
+void keys2Confirm(ubyte *u, ubyte *v) {
 	int flag = 0;
-	ubyte t1, t;
+	ubyte zOneBit2Compare, zOneBit;
+
+	//falls die Werte von aktuelle position beide 0 sind, bekommt man folgenden bit von 'z'
 	*u &= s2;
 	*v &= s2;
-	t1 = bitsCheck(*u, *v);
+	zOneBit2Compare = getBitValueOfFEAL(*u, *v);
 
+	//falls die Werte von aktuelle position 0 und 1 sind,...
 	*u &= s2;
 	*v |= s1;
-	t = bitsCheck(*u, *v);
-	if (t != t1) {
+	zOneBit = getBitValueOfFEAL(*u, *v);
+	if (zOneBit != zOneBit2Compare) {
 		flag = 1;
 	}
 
+	//falls die Werte von aktuelle position 1 und 0 sind,...
 	*u |= s1;
 	*v &= s2;
-	t = bitsCheck(*u, *v);
+	zOneBit = getBitValueOfFEAL(*u, *v);
 
-	if (t == t1 && flag == 0) {
+
+	//Bestimmt aktuelle Bit-Value von v und u
+	if (zOneBit == zOneBit2Compare && flag == 0) {
 		*u |= s1;
 		*v |= s1;
 		return;
-	} else if (t == t1 && flag == 1) {
+	} else if (zOneBit == zOneBit2Compare && flag == 1) {
 		*u &= s2;
 		*v |= s1;
 		return;
@@ -89,9 +96,10 @@ int main(int argc, char **argv) {
 
 	ubyte u = 0, v = 0, w = 0;
 
+	// Nur 7-bits Keys können bestimmt sein
 	int i;
 	for (i = 1; i <= 7; i++) {
-		bCheck(&u, &v);
+		keys2Confirm(&u, &v);
 		if (i % 2 == 0) {
 			k1 += (s1 & (~u));
 			k2 += (s1 & (~v));
@@ -103,15 +111,10 @@ int main(int argc, char **argv) {
 		s2 = (s2 << 1) | 1;
 	}
 
-	//if (bitsCheck(u, v) >> 7 == 1)
-	//	v = 0x80 | v;
 
-	//printf("%0x,%0x\n", u, v);
 	w = calc_f(u, v);
 	k3 = w ^ rot2(((u ^ k1) + (v ^ k2) + 1) % 256);
 
-	//printf("%x,",w);
-	//printf("%x\n",Feal_G(k1,k2,k3,u,v));
 
 	printf("Lösung: $%02x $%02x $%02x: %s", k1, k2, k3,
 			Feal_CheckKey(k1, k2, k3) ? "OK!" : "falsch");
